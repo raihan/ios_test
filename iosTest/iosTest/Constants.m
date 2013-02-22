7188d76045dbbc30bd995826aba7f6c38ca7fbba
raihan <raihan.zbr@gmail.com>
2013-02-22 23:54:21 +0600

    URL changed

--- iosTest/iosTest/Constants.m
+++ iosTest/iosTest/Constants.m
@@ -11,8 +11,7 @@
 @implementation Constants
 
 NSString * const NOTIF_GET_JSON_DATA_DONE = @"com.iosTest.getjsondatadone";
-//NSString *jsonUrlString = @"https://gist.github.com/anonymous/4680060/raw/aac6d818e7103edfe721e719b1512f707bcfb478/sample.json";
-NSString *jsonUrlString = @"file:///Users/abdullahmdzubair/Downloads/sample.json";
+NSString *jsonUrlString = @"https://gist.github.com/anonymous/4680060/raw/aac6d818e7103edfe721e719b1512f707bcfb478/sample.json";
 NSString *entitiesNameOfMyFiles = @"MyFileContents";
 NSString *entitiesNameOfSharedFiles = @"SharedFileContents";
 int maxNumRows = 20;
